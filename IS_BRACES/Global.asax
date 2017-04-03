af94407e9c93f7b38e933056ee69bc2eec378ba7
lihmad <lihmadam@gmail.com>
2017-04-03 02:13:54 +0200

    Add project files.

---
+++ IS_BRACES/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="IS_BRACES.WebApiApplication" Language="C#" %>
---
+++ IS_BRACES/Startup.cs
@@ -0,0 +1,10 @@
+﻿using Owin;
+namespace IS_BRACES
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+        }
+    }
+}
