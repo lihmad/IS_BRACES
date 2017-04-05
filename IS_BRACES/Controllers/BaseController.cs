84dcab8b6468a74716960e86fb638c747ca4177e
lihmad <lihmadam@gmail.com>
2017-04-05 03:13:57 +0200

    Napojeni na DB

---
+++ IS_BRACES/Controllers/BaseController.cs
@@ -0,0 +1,34 @@
+﻿using IS_BRACES.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IS_BRACES.Controllers
+{
+    public class BaseController : Controller
+    {
+        private DB_Model _db_Model;
+
+        protected DB_Model DB {
+            get {
+                if (_db_Model == null) {
+                    _db_Model = new DB_Model();
+                }
+                return (_db_Model);
+            }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing) {
+                if (_db_Model != null) {
+                    _db_Model.Dispose();
+                    _db_Model = null;
+                }
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- IS_BRACES/Controllers/HomeController.cs
+++ IS_BRACES/Controllers/HomeController.cs
@@ -1,16 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using IS_BRACES.Models;
 
 namespace IS_BRACES.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : BaseController
     {
         public ActionResult Index()
         {
             ViewBag.Title = "Home Page";
+            ViewBag.G = DB.Destinace.First().Zeme;
 
             return View();
         }
